41a5fe8e0702590562f86eeb232a0ad33b7464c3
Tessa Prince <tprince119@academic.rrc.ca>
2022-10-08 16:50:28 -0500

    first steps for seed file, code to create pokedex and pokemon

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,31 @@ require "csv"
 
 Pokemon.delete_all
 Pokedex.delete_all
-Pokemon_type.delete_all
+PokemonType.delete_all
 Type.delete_all
 
 filename = Rails.root.join("db/Pokemon.csv")
 puts "Loading the Pokemon from the csv file: #{filename}"
+
+csv_data = File.read(filename)
+pokemon = CSV.parse(csv_data, headers:true, encoding: "utf-8")
+
+pokemon.each do |p|
+  pokedex_number = Pokedex.find_or_create_by(number: p["#"])
+  if pokedex_number && pokedex_number.valid?
+    #creates the pokemon
+    pokemon = pokedex_number.pokemons.create(
+      name: p["Name"],
+      hit_points: p["HP"],
+      attack_strength: p["Attack"],
+      defensive_strength: p["Defense"],
+      special_attack_strength: p["Sp. Atk"],
+      special_defensive_strength: p["Sp. Def"]
+    )
+    puts "Invalid Pokemon #{p['Name']}" unless pokemon&.valid?
+  else
+    puts "Invalid pokedex #{p['#']} for pokemon #{p["Name"]}"
+  end
+end
+puts "Created #{Pokedex.count} Pokedex entries."
+puts "Created #{Pokemon.count} Pokemon."
