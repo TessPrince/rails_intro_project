dfe2a2d038c904bf293e3d056bf17e45cc26e699
Tessa Prince <tprince119@academic.rrc.ca>
2022-10-08 13:28:36 -0500

    added back the validation for pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,8 @@
 class Pokemon < ApplicationRecord
   belongs_to :pokedex
   belongs_to :pokemon_types
+
+  validates :name, :hit_points, :attack_strength, :defensive_strength, :special_attack_strength, :special_defensive_strength, presence:true
+  validates :name, uniqueness: true
+  validates :hit_points, :attack_strength, :defensive_strength, :special_attack_strength, :special_defensive_strength, numericality {only_integer: true}
 end
