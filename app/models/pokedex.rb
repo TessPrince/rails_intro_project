13d04fc217d412b8ba28a0cd10b433d551510add
Tessa Prince <tprince119@academic.rrc.ca>
2022-10-08 12:43:39 -0500

    Added Pokemon model with Pokedex association with validations

--- app/models/pokedex.rb
+++ app/models/pokedex.rb
@@ -1,2 +1,4 @@
 class Pokedex < ApplicationRecord
+  has_many :pokemons
+  validates :number, presence: true, uniqueness: true
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,8 @@
 class Pokemon < ApplicationRecord
+  belongs_to :pokedex
+
+  validates :hit_points, :attack_strength, :defensive_strength, :special_attack_strength, :special_defense_strength, :name, presence: true
+  validates :name, uniqueness: true
+  validates :hit_points, :attack_strength, :defensive_strength, :special_attack_strength, :special_defense_strength, numericality: {only_integer: true}
+
 end
