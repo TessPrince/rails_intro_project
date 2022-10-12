01614c45f876f57bcdfa51595f414fa622ac612f
Tessa Prince <tprince119@academic.rrc.ca>
2022-10-12 17:04:51 -0500

    re-made about page to fix bulma

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root "pokemon#index"
 
   get "pokemon", to: "pokemon#index", as: "pokemon"
-  #get "pokedex:id", to: "pokedex#show", as: "pokedex", constraints: { id: /\d+/ }
   get "about", to: "about#index", as: "about"
 
  resources :pokemon, only: [:index, :show]
