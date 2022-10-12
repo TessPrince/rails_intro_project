84989c48bd3de5cc5bf40031c5c12e6a861565df
Tessa Prince <tprince119@academic.rrc.ca>
2022-10-12 18:27:07 -0500

    nav bar moved to use layout instead

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   get "pokemon", to: "pokemon#index", as: "pokemon"
   get "about", to: "about#index", as: "about"
 
- resources :pokemon, only: [:index, :show]
- resources :pokedex, only: [:show]
+  resources :pokemon, only: [:index, :show]
+  #pokemon/search/(:format)
+  # collection do
+  #       get get "search"
+  #     end
+  #   end
+
+  resources :pokedex, only: [:show]
 end
