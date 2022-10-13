0eb18286ad8dfe6bb95aeddf9f504e2590557f2d
Tessa Prince <tprince119@academic.rrc.ca>
2022-10-12 19:20:23 -0500

    fixed navigation and links to specific pokemon

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -6,4 +6,10 @@ class PokemonController < ApplicationController
   def show
     @pokemon = Pokemon.find(params[:id])
   end
+
+  def search
+    wildcard ="%#{params[:keywords]}%"
+
+    @pokemons = Pokemon.where("name LIKE ?", wildcard)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
-  root "pokemon#index"
+  root to: "pokemon#index"
 
-  get "pokemon", to: "pokemon#index", as: "pokemon"
   get "about", to: "about#index", as: "about"
 
-  resources :pokemon, only: [:index, :show]
-  #pokemon/search/(:format)
-  # collection do
-  #       get get "search"
-  #     end
-  #   end
+  resources :pokemon, only: [:index, :show] do
+    #pokemon/search/(:format)
+    collection do
+        get "search"
+      end
+    end
 
   resources :pokedex, only: [:show]
 end
